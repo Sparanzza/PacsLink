7b6a52527086a0f987edc9a2419cc3445ec1caea
Aurelio Muñoz Hijano <aurelio@sparanzza.com>
2025-07-29 23:13:51 +0200

    Added basic Worker service in DicomServer.SCP.csproj

--- Server/DicomServer.SCP/Program.cs
+++ Server/DicomServer.SCP/Program.cs
@@ -1,14 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using FellowOakDicom;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-Console.WriteLine("Init DicomServer.SCP!");
+using Microsoft.Extensions.Logging;
 
 var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
+        logging.AddDebug();
+    })
     .ConfigureServices(services =>
     {
         services.AddFellowOakDicom();
+        services.AddHostedService<Worker>();
     })
     .Build();
 
@@ -16,3 +23,18 @@ var host = Host.CreateDefaultBuilder(args)
 DicomSetupBuilder.UseServiceProvider(host.Services);
 
 await host.RunAsync();
+
+public class Worker(ILogger<Worker> logger) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Starting DicomServer.SCP...");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Stopping DicomServer.SCP...");
+        return Task.CompletedTask;
+    }
+}
